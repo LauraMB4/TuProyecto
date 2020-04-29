95919b93a80253e2dfb29b915582278b8d1b1d8b
user <user@DESKTOP-SUVLE5S>
2020-04-29 19:02:28 +0200

    Prueba 2

--- sumador/src/sumador/ASumar.java
+++ sumador/src/sumador/ASumar.java
@@ -10,10 +10,13 @@ public class ASumar {
 	 * Mostrar.
 	 *
 	 * @param string con la cifra 
-	 * @return string = ella misma por ser de una cifra
+	 * @return string = ella misma por ser de una cifra. En caso de ser negativa devuelve una cadena vacía.
 	 */
 	public String mostrar(String string) {
 		// TODO Auto-generated method stub
+		if (string.charAt(0)=='-')
+			return ("");
+
 		return (string + " = " + string);
 	}
 
--- sumador/test/sumador/TestASumar.java
+++ sumador/test/sumador/TestASumar.java
@@ -7,13 +7,23 @@ import org.junit.jupiter.api.Test;
 class TestASumar {
 
     /**
-     * Test para una cifra.
+     * Test para una cifra de un dígito.
      */
     @Test
     public void testUnaCifra() {
         ASumar sumi = new ASumar();
         String resultado = sumi.mostrar("5");
         assertEquals("5 = 5",resultado);
+    }
+    
+    /**
+     * Test para una cifra negativo.
+     */
+    @Test
+    public void testNegativo() {
+        ASumar sumi = new ASumar();
+        String resultado = sumi.mostrar("-5");
+        assertEquals("",resultado );
     }
 
 }
