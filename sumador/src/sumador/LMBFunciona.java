01d85654844b7ac9229166b4d738c27eb43cb347
user <user@DESKTOP-SUVLE5S>
2020-04-29 19:56:19 +0200

    Finalizacion

---
+++ sumador/src/sumador/LMBFunciona.java
@@ -0,0 +1,14 @@
+package sumador;
+
+public class LMBFunciona {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ASumar s = new ASumar();
+		
+		System.out.println(s.mostrar("5"));
+		System.out.println(s.mostrar("-7"));
+		System.out.println(s.mostrar("3433"));
+	}
+
+}
